9ce56e96ebc4830df091949167b377f3d8eb8da7
Corey Ferguson <corey_ferguson@intuit.com>
2015-10-25 11:57:59 -0700

    Prototype complete.

--- public/index.js
+++ public/index.js
@@ -1,56 +1,106 @@
+// config
+var totalNumberOfAtoms = 495;
+var boxBuffer = 15;
+var atomRadius = 5;
+var atomPadding = 7;
+var loopTimeInMilliseconds = 100;
+var percentChanceOfGoingThroughSlit = 0.01;
 
-var engineIterationTimeInMilliseconds = 1000;
-var numberOfAtoms = 20;
-var atoms = [];
+// buffer + padding + rows + columns
+var boxLeft = d3.select('#boxLeft');
+var boxRight = d3.select('#boxRight');
+var boxHeight = boxLeft.node().getBoundingClientRect().height;
+var boxWidth = boxLeft.node().getBoundingClientRect().width;
+if (boxHeight !== boxRight.node().getBoundingClientRect().height || 
+	boxWidth !== boxRight.node().getBoundingClientRect().width) {
+	throw new Error('Boxes are not equal in height and width.');
+}
+var boxRows = Math.floor(( 
+	(boxHeight - boxBuffer*2 - atomPadding*2) / 
+	(atomPadding*2) 
+) + 1);
+var boxColumns = Math.floor(( 
+	(boxWidth - boxBuffer*2 - atomPadding*2) / 
+	(atomPadding*2) 
+) + 1);
+var maximumAtomsWithinSpaceGiven = boxRows * boxColumns;
+if (totalNumberOfAtoms > maximumAtomsWithinSpaceGiven) {
+	throw new Error(
+		'Number of atoms specified (' + 
+			totalNumberOfAtoms + 
+			') cannot fit within limited space (' + 
+			maximumAtomsWithinSpaceGiven + 
+			').');
+}
 
-var engine = function() {
-	moveAtoms();
-	paintAtoms();
-	setTimeout(engine, engineIterationTimeInMilliseconds);
-};
-
+var atomsLeft = [];
+var atomsRight = [];
 init();
-engine();
+paint();
+
+var loop = function() {
+	moveAtoms();
+	paint();
+	setTimeout(loop, loopTimeInMilliseconds);
+}
+setTimeout(loop, loopTimeInMilliseconds);
 
 function init() {
-	atoms = [];
-	for (var c=0; c<numberOfAtoms; c++) {
-		atoms.push({
-			id: guid(),
-			side: 'left'
-		});
+	for (var c=0; c<totalNumberOfAtoms; c++) {
+		atomsLeft.push(guid())
 	}
 }
 
 function moveAtoms() {
-	for (var c=0; c<atoms.length; c++) {
-		if (Math.random() < 0.1) {
-			if (atoms[c].side === 'left') {
-				atoms[c] = {id: guid(), side: 'right'};
-			} else if (atoms[c].side === 'right') {
-				atoms[c] = {id: guid(), side: 'left'};
-			}
+	var newAtomsLeft = [];
+	var newAtomsRight = [];
+	for (var c=0; c<atomsLeft.length; c++) {
+		var atom = atomsLeft[c];
+		if (Math.random() < percentChanceOfGoingThroughSlit) {
+			newAtomsRight.push(guid());
+		} else {
+			newAtomsLeft.push(guid());
 		}
 	}
+	for (var c=0; c<atomsRight.length; c++) {
+		var atom = atomsRight[c];
+		if (Math.random() < percentChanceOfGoingThroughSlit) {
+			newAtomsLeft.push(guid());
+		} else {
+			newAtomsRight.push(guid());
+		}
+	}
+	atomsLeft = newAtomsLeft;
+	atomsRight = newAtomsRight;
 }
 
-function paintAtoms() {
-	// config
-	var atomRadius = 5;
-	// paint
-	var svg = d3.select('svg');
-	var circle = svg.selectAll('circle').data(atoms, function(atom) { return atom.id; });
-	var circleEnter = circle.enter().append('circle');
-	circleEnter.attr('r', atomRadius);
-	circleEnter.attr('cx', function(atom, index) {
-		if (atom.side === 'left') {
-			return 125;
-		} else if (atom.side === 'right') {
-			return 375;
-		}
-	});
-	circleEnter.attr('cy', function(atom, index) { return 50+index*atomRadius*4; });
-	circle.exit().remove();
+function paint() {
+	// left box
+	var circleLeft = boxLeft.selectAll('circle')
+		.data(atomsLeft, function(atom) { return atom; });
+	circleLeft.enter().append('circle')
+		.attr('style', 'fill:blue')
+		.attr('r', atomRadius)
+		.attr('cx', function(atom, index) {
+			return boxBuffer + atomPadding + 2*atomPadding*( Math.floor(index/boxRows) );
+		})
+		.attr('cy', function(atom, index) {
+			return boxBuffer + atomPadding + 2*atomPadding*(index%boxRows);
+		});
+	circleLeft.exit().remove();
+	// right box
+	var circleRight = boxRight.selectAll('circle')
+		.data(atomsRight, function(atom) { return atom; });
+	circleRight.enter().append('circle')
+		.attr('style', 'fill:blue')
+		.attr('r', atomRadius)
+		.attr('cx', function(atom, index) {
+			return boxBuffer + atomPadding + 2*atomPadding*( Math.floor(index/boxRows) );
+		})
+		.attr('cy', function(atom, index) {
+			return boxBuffer + atomPadding + 2*atomPadding*(index%boxRows);
+		});
+	circleRight.exit().remove();
 }
 
 function guid() {
